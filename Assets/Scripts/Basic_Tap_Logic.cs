fb9f2b137137a9a60d3df35e1ab2266422ac674f
batomow <carlosa_miranda@outlook.com>
2017-05-17 13:22:18 -0700

    estoy empesando con la logica basica, aun estoy viendo como implementar las notas musicales, cree un tipo de dato Son_taps que contiene una array de 2dimensiones de bools, el concepto es que funcione como los pianos que se tocan solos pero haciendo calculos con el tempo y la duracion de la cancion para que corra en sincronia, si es true dispara una nota musical

--- Assets/Scripts/Basic_Tap_Logic.cs
+++ Assets/Scripts/Basic_Tap_Logic.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class Basic_Tap_Logic : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject NOTE_PREFAB; 
+	public Vector3[] spawnPositions; 
+	public Song_Taps song1; 
+
+
+	void Awake(){
+		spawnPositions = new Vector3[3];
+		song1 = new Song_Taps (10f); 
+		///here are the song notes you must play
+		song1.taps[0,0] = false; song1.taps[0,1] = true; song1.taps[0,2] = false; //1
+		song1.taps[1,0] = false; song1.taps[1,1] = true; song1.taps[1,2] = false; //1
+		song1.taps[2,0] = false; song1.taps[2,1] = false; song1.taps[2,2] = false; //1
+		song1.taps[3,0] = false; song1.taps[3,1] = false; song1.taps[3,2] = false; //1
+		song1.taps[4,0] = false; song1.taps[4,1] = false; song1.taps[4,2] = false; //1
+		song1.taps[5,0] = false; song1.taps[5,1] = false; song1.taps[5,2] = false; //1
+		song1.taps[6,0] = false; song1.taps[6,1] = false; song1.taps[6,2] = false; //1
+		song1.taps[7,0] = false; song1.taps[7,1] = false; song1.taps[7,2] = false; //1
+		song1.taps[8,0] = false; song1.taps[8,1] = false; song1.taps[8,2] = false; //1
+		song1.taps[9,0] = false; song1.taps[9,1] = false; song1.taps[9,2] = false; //1
+
+		///here int ends
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Start(){
+		StartCoroutine (launchTaps()); 
 	}
+
+	IEnumerator launchTaps(){
+		while (true) {
+			for (int i = 0; i < song1.songRate; i++) {
+				if (song1.taps [i, 0] == true) {
+					Note_Logic go=Instantiate (NOTE_PREFAB, spawnPositions [0], Quaternion.identity) as Note_Logic; 
+					go.move (); 
+				}
+				if (song1.taps [i, 1] == true) {
+					Note_Logic go=Instantiate (NOTE_PREFAB, spawnPositions [1], Quaternion.identity)as Note_Logic; 
+					go.move (); 
+				}
+				if (song1.taps [i, 2] == true) {
+					Note_Logic go=Instantiate (NOTE_PREFAB, spawnPositions [2], Quaternion.identity	) as Note_Logic; 
+					go.move (); 
+				}
+			}
+
+			yield return new WaitForSeconds (song1.tempo); 
+		}
+	}
+
 }
---
+++ Assets/Scripts/Note_Logic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note_Logic : MonoBehaviour {
+
+
+	IEnumerator StartMoving(int rate){
+		while (true) {
+			this.transform.Translate (Vector3.left * rate); 
+			yield return new WaitForSeconds (Time.fixedDeltaTime); 
+		}
+	}
+
+	public void move(){
+		StartCoroutine (StartMoving ()); 
+	}
+
+	public void stop(){
+		StopCoroutine (StartMoving ()); 
+	}
+}
