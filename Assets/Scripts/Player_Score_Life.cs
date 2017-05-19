7a43e252f4afb547f2a98e8942067586d5f5b395
batomow <carlosa_miranda@outlook.com>
2017-05-19 14:00:17 -0700

    mayor changes, cambie los box colliders y agrege el score

---
+++ Assets/Scripts/Player_Score_Life.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine; 
+public class Player_Score_Life {
+
+	static public float LIFE; 
+	static public float SCORE; 
+
+
+}
---
+++ Assets/Scripts/Touch_Thingies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch_Thingies : MonoBehaviour {
+
+
+	void Awake(){
+		Player_Score_Life.LIFE = 100f; 
+		Player_Score_Life.SCORE = 0; 
+	}
+
+
+	void OnTriggerEnter(Collider something){
+		if (something.tag == "Note") {
+			Player_Score_Life.SCORE+=2;
+			Player_Score_Life.LIFE = Mathf.Clamp (Player_Score_Life.LIFE + 1, 0, 100); 
+			Destroy (something.gameObject); 
+		}
+		if (something.tag == "Obstacle") {
+			Player_Score_Life.LIFE = Mathf.Clamp (Player_Score_Life.LIFE - 2, 0, 100); 
+			Player_Score_Life.SCORE--;
+			Destroy (something.gameObject); 
+		}
+
+		Debug.Log (Player_Score_Life.LIFE + " " + Player_Score_Life.SCORE); 
+	}
+
+
+
+
+}
