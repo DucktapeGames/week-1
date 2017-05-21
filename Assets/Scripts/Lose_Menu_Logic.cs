0b4fec584cf6cbb64a464d9d1e61601fa38992b9
batomow <carlosa_miranda@outlook.com>
2017-05-21 15:29:38 -0700

    added a losing sequence based on the pplayers x positions

---
+++ Assets/Scripts/Lose_Menu_Logic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lose_Menu_Logic : MonoBehaviour {
+
+	public Pause pauseLogicObject;
+	public GameObject loseScree; 
+	public Transform player; 
+
+
+	void StartLoseSequence(){
+		pauseLogicObject.enabled = false; 
+		loseScree.SetActive (true); 
+	}
+
+	void Update(){
+		if (player.position.x <=-35f) {
+			StartLoseSequence (); 
+		}
+	}
+
+
+
+}
