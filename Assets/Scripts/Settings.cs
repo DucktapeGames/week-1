2612553851bde6f5d95c67d5632bdeab6d04b0c3
Javier Meza <javenmeza@gmail.com>
2017-05-21 17:55:11 -0600

    Adds volume management

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour {
+	public AudioMixer MasterMixer;
+
+	public float Volume {
+		set {
+			MasterMixer.SetFloat("MusicVolume", Mathf.Clamp(value, -80, 20));
+		}
+
+		get {
+			float val;
+			MasterMixer.GetFloat ("MusicVolume", out val);
+			return val;
+		}
+	}
+}
