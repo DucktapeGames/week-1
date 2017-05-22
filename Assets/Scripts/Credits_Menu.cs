a02eb6aebb8c82864225973bb9a0db507cc6017e
julio177 <julioc.aguilarv@gmail.com>
2017-05-21 20:33:36 -0500

    Termina credits scene

---
+++ Assets/Scripts/Credits_Menu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits_Menu : MonoBehaviour {
+
+	public void Menu() {
+		SceneManager.LoadScene (0);
+	}
+}
